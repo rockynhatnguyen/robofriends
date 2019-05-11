03b3cd9af857db6362a2fb772ad69f5a85828430
Nhat Rocky Nguyen <nhatrockynguyen@gmail.com>
2019-05-10 19:20:47 -0700

    Updates

---
+++ precache-manifest.53854d414cdb4ca3216fbda12c135428.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6027ea3cfcab336691a8dee2d0b2bdab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c6e1ac927aae1219aa28",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5aea3fe02b6ae99ff14d",
+    "url": "/robofriends/static/css/main.5cec03d5.chunk.css"
+  },
+  {
+    "revision": "c6e1ac927aae1219aa28",
+    "url": "/robofriends/static/js/2.4d43d901.chunk.js"
+  },
+  {
+    "revision": "5aea3fe02b6ae99ff14d",
+    "url": "/robofriends/static/js/main.bc578af7.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "f1ac2dd1e4aa64afad1d0c5b866e6a68",
+    "url": "/robofriends/static/media/ProximaNova-Light.f1ac2dd1.woff"
+  },
+  {
+    "revision": "55358afacdc49f0a021aeab22018183d",
+    "url": "/robofriends/static/media/ProximaNova-Regular.55358afa.woff"
+  }
+]);
